61c1b495a4191e3f65f834bef81ed5c5c13c1d67
Alormor <abralorenzo@gmail.com>
2025-12-11 08:36:59 +0100

    Not working

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,17 +1,42 @@
+import { useState } from "react";
 import './assets/styles/searchBar.css'
 
-export default function SearchBar(){
+export default function SearchBar({onSearch}){
+    const [query, setQuery] = useState("");
+    const [year, setYear] = useState("");
+    const [type, setType] = useState("");
+
+    const handleSearch = () => {
+        onSearch(query, year, type);
+    }
+
+
     return(
         <div className="searchBar">
-            <input className="input-search" placeholder="Search..."/>
+            <input 
+                className="input-search"
+                placeholder="Search..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            />
 
-            <button className="btn-search">
-                <i class="fa fa-search"></i>
+            <button className="btn-search" onClick={handleSearch}>
+                <i className="fa fa-search"></i>
             </button>
 
             <div className="filters">
-                <input className="input-year" placeholder="Year"/>
-                <select className="select-type" name="type">
+                <input 
+                    className="input-year" 
+                    placeholder="Year"
+                    value={year}
+                    onChange={(e) => setYear(e.target.value)}
+                />
+                <select 
+                    className="select-type" 
+                    name="type"
+                    value={type}
+                    onChange={(e) => setType(e.target.value)}
+                >
                     <option value="">-- Type --</option>
                     <option value="movie">Movies</option>
                     <option value="series">Series</option>
--- src/SearchPage.jsx
+++ src/SearchPage.jsx
@@ -5,22 +5,36 @@ import SearchBar from "./SearchBar";
 
 export default function SearchPage() {
     const [movies, setMovies] = useState([]);
+    const [query, setQuery] = useState("star");
+    const [year, setYear] = useState("");
+    const [type, setType] = useState("");
     const [page, setPage] = useState(1);
 
     useEffect(() =>{
-        fetch("https://www.omdbapi.com/?apikey=dfe7b98e&s=star").then(
-        response => response.json()).then(data =>{
-            fetchedMovies(data.Search);
-        });
-    }, [page]);
+        console.log(query)
+        if(query != undefined){
+            fetch(`https://www.omdbapi.com/?apikey=dfe7b98e&s=${query}&page=${page}&y=${year}&type=${type}`).then(
+            response => response.json()).then(data =>{
+                fetchedMovies(data.Search);
+            });
+        }
+    }, [query]);
 
     function fetchedMovies(movieList){
         setMovies([...movies, ...movieList]);
     }
 
+    const search = (newQuery, newYear, newType) => {
+        setMovies([]);
+        setQuery(newQuery);
+        setYear(newYear);
+        setType(newType);
+        setPage(1);
+    }
+
     return (
         <>
-            <SearchBar/>
+            <SearchBar onSearch={search()}/>
             <ShowMovies movies={movies} />
         </>
     )
